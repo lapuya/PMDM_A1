948d4ec1127691b98e49a1ebb3ca4be231db1c02
laurenceapuya <laurence.apuya.p@gmail.com>
2022-02-06 10:55:22 +0100

    Registro y login hechos. Falta personalizacion de la app

---
+++ app/src/main/java/com/example/todolist/LoginActivity.java
@@ -0,0 +1,69 @@
+package com.example.todolist;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.todolist.db.TaskDbHelper;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText name,pass;
+    TaskDbHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        name = (EditText)findViewById(R.id.username);
+        pass = (EditText)findViewById(R.id.password);
+        db = new TaskDbHelper(this);
+    }
+
+
+    public void login(View view){
+        String account = name.getText().toString();
+        String password = pass.getText().toString();
+        if(account.isEmpty() || password.isEmpty()){
+            Toast toast = Toast.makeText(this,"Fill in the fields",Toast.LENGTH_LONG);
+            toast.show();
+
+        }else if(db.validateAccount(account, password)){
+            Toast toast = Toast.makeText(this,"Wrong credentials",Toast.LENGTH_LONG);
+            toast.show();
+        }else{
+            Intent intent = new Intent(this,MainActivity.class);
+            intent.putExtra("user",account);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public void registerUser(View view){
+        String account = name.getText().toString();
+        String password = pass.getText().toString();
+        if(account.isEmpty() || password.isEmpty()){
+            Toast toast = Toast.makeText(this,"Fill in the fields",Toast.LENGTH_LONG);
+            toast.show();
+        }else if(db.checkAccount(account)){
+            Toast toast = Toast.makeText(this,"A user already exists",Toast.LENGTH_LONG);
+            toast.show();
+        }else{
+            db.insertNewAccount(account,password);
+            Toast toast = Toast.makeText(this,"Usuario creado",Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/todolist/db/TaskDbHelper.java
+++ app/src/main/java/com/example/todolist/db/TaskDbHelper.java
@@ -1,8 +1,12 @@
 package com.example.todolist.db;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class TaskDbHelper extends SQLiteOpenHelper {
 
@@ -16,12 +20,37 @@ public class TaskDbHelper extends SQLiteOpenHelper {
                 TaskContract.TaskEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 TaskContract.TaskEntry.COL_TASK_TITLE + " TEXT NOT NULL);";
 
+
         db.execSQL(createTable);
+        db.execSQL("CREATE TABLE ACCOUNTS (NAME TEXT PRIMARY KEY NOT NULL, PASSWORD TEXT NOT NULL)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TaskContract.TaskEntry.TABLE);
         onCreate(db);
+    }
+
+    public boolean validateAccount(String username, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor= db.rawQuery("SELECT * FROM ACCOUNTS WHERE NAME=? AND PASSWORD=?",new String[]{String.valueOf(username),String.valueOf(password)});
+        int regs = cursor.getCount();
+        return regs <= 0;
+    }
+
+    public boolean checkAccount(String username){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("Select * from ACCOUNTS where NAME = ?", new String[]{username});
+        int num = cursor.getCount();
+        return num > 0;
+    }
+
+    public void insertNewAccount(String account, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put("NAME", account);
+        contentValues.put("PASSWORD", password);
+        db.insert("ACCOUNTS", null, contentValues);
+        db.close();
     }
 }
